b579472d6f7cef7786b66cea9e9fb6c425dbf3b4
Han qin Loke <hloke@u82z05s06.42kl.edu.my>
2022-07-11 15:42:14 +0800

    [Addition] ex02 pointer and reference done

---
+++ c01/ex02/main.cpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hloke <hloke@student.42kl.edu.my>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/08 15:06:51 by hloke             #+#    #+#             */
+/*   Updated: 2022/07/11 15:40:51 by hloke            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+/*
+ * https://www.geeksforgeeks.org/passing-by-pointer-vs-passing-by-reference-in-c/
+ */
+int main(void)
+{
+	std::string str = "HI THIS IS BRAIN";
+	std::string *stringPTR = &str;
+	std::string &stringREF = str;
+
+	std::cout << "Address of str: " << &str << std::endl;
+	std::cout << "Address of stringPTR: " << stringPTR << std::endl;
+	std::cout << "Address of stringREF: " << &stringREF << std::endl << std::endl;
+
+	std::cout << "Value of str: " << str << std::endl;
+	std::cout << "Value of stringPTR: " << *stringPTR << std::endl;
+	std::cout << "Value of stringREF: " << stringREF << std::endl;
+	return (0);
+}

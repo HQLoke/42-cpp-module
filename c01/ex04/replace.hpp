021fec59bea106dca26476403b18d12abd176927
Han qin Loke <hloke@u82z05s06.42kl.edu.my>
2022-07-11 22:50:18 +0800

    [Addition] ex04 ifstream, ofstream and string methods done.

---
+++ c01/ex04/replace.hpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   replace.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hloke <hloke@student.42kl.edu.my>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/11 19:08:17 by hloke             #+#    #+#             */
+/*   Updated: 2022/07/11 22:16:38 by hloke            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef REPLACE_HPP
+# define REPLACE_HPP
+
+# include <fstream>
+# include <iostream>
+# include <sstream>
+# include <string>
+
+#endif

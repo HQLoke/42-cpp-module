d201176ded821aa837833abe1e4a5d1e514adb2a
Han qin Loke <hloke@u82z05s06.42kl.edu.my>
2022-07-12 14:02:19 +0800

    [Addition] c01/ex04 added test cases prototype mark 1

--- c01/ex04/main.cpp
+++ c01/ex04/main.cpp
@@ -6,46 +6,55 @@
 /*   By: hloke <hloke@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/11 18:50:34 by hloke             #+#    #+#             */
-/*   Updated: 2022/07/11 22:48:52 by hloke            ###   ########.fr       */
+/*   Updated: 2022/07/12 12:46:21 by hloke            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "replace.hpp"
 
-int main(int ac, char **av)
+static void	replacing(char **av, std::ifstream &infile, std::ofstream &outfile)
 {
-	if (ac != 4)
-	{
-		std::cerr << "The program opens the file <filename> and copies its\n"
-		          << "content into a newfile, replacing every occurrence of\n"
-				  << "s1 with s2.\n";
-		std::cerr << "Usage: ./replace filename s1 s2\n";
-		return (EXIT_FAILURE);
-	}
-	
-	std::ifstream	infile(av[1]);
-	std::ofstream	outfile(std::string(av[1]).append(".replace"), std::ios::app);
 	std::string		content;
+	std::string		s1(av[2]);
+	std::string		s2(av[3]);
+	size_t			found;
+	size_t			index;
 
+	index = 0;
 	while (std::getline(infile, content))
 	{
-		std::string	s1(av[2]);
-		std::string	s2(av[3]);
-		size_t		found;
-		size_t		index = 0;
-		
-		do
+		while ((found = content.find(s1, index)) != std::string::npos)
 		{
-			found = content.find(s1, index);
-			if (found != std::string::npos)
-			{
-				content.erase(found, s1.length());
-				content.insert(found, s2);
-			}
+			content.erase(found, s1.length());
+			content.insert(found, s2);
 			index = found + s2.length();
-		} while (found != std::string::npos);
+		} 
 		outfile << content << std::endl;
 	}
+}
+
+int main(int ac, char **av)
+{
+	std::ifstream	infile;
+	std::ofstream	outfile;
+	
+	if (ac != 4)
+	{
+		std::cerr << "The program opens the file <filename> and copies its "
+		          << "content into a newfile, replacing every occurrence of "
+				  << "s1 with s2.\n";
+		std::cerr << "Usage: ./replace filename s1 s2\n";
+		exit (EXIT_FAILURE);
+	}
+	infile.open(av[1], std::ios::in);
+	if (infile.is_open() == false)
+	{
+		std::cerr << "Error: file <" << av[1] << "> does not exist.\n";
+		exit (EXIT_FAILURE);
+	}
+	else
+		outfile.open(std::string(av[1]).append(".replace"), std::ios::out);
+	replacing(av, infile, outfile);
 	infile.close();
 	outfile.close();
 	return (0);

0d39e2c4ca38bc833a2cf051f5538ca835e05a2d
Han qin Loke <hloke@u82z05s06.42kl.edu.my>
2022-07-13 14:05:10 +0800

    [Addition] c02/ex01 fixed-point representation and overloading done

--- c02/ex01/Fixed.cpp
+++ c02/ex01/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: hloke <hloke@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/12 21:03:43 by hloke             #+#    #+#             */
-/*   Updated: 2022/07/12 21:24:20 by hloke            ###   ########.fr       */
+/*   Updated: 2022/07/13 14:04:10 by hloke            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,93 @@
 //? Default constructor, copy constructor, copy assignment operator & destructor
 
 Fixed::Fixed()
-{}
-
-Fixed::Fixed(const Fixed &f)
 {
-	
+	std::cout << "Default constructor called\n";
 }
 
-Fixed& Fixed::operator= (const Fixed &f)
+Fixed::Fixed(const int int_val)
 {
-	
+	std::cout << "Int constructor called\n";
+	_fixedPointVal = int_val << _sFracBits;
 }
 
-Fixed::~Fixed(){}
+/*
+ * 42.5 (float) = 101010.10000000 (fixed-point with 8 fractional bits)
+ * Bitwise left shift N times = multiplying by 2^N	(42.5 * 2^8 = 10880)
+ * Bitwise right shift N times = dividing by 2^N
+ * Since precision is 8 fractional bits only, we need to round off to the nearest int.
+ */ 
+Fixed::Fixed(const float f_val)
+{
+	std::cout << "Float constructor called\n";
+	float	shift_val;
+
+	shift_val = f_val * pow(2, _sFracBits);
+	_fixedPointVal = roundf(shift_val);
+}
+
+Fixed::Fixed(const Fixed &fixed)
+{
+	std::cout << "Copy constructor called\n";
+	*this = fixed;
+}
+
+Fixed& Fixed::operator= (const Fixed &fixed)
+{
+	std::cout << "Copy assignment operator called\n";
+	if (this == &fixed)
+		return (*this);
+	this->_fixedPointVal = fixed.getRawBits();
+	return (*this);
+}
+
+Fixed::~Fixed()
+{
+	std::cout << "Destructor called\n";
+}
 
 //? Public methods
 
+int		Fixed::getRawBits(void) const
+{
+	return (this->_fixedPointVal);
+}
+
+void	Fixed::setRawBits(int const raw)
+{
+	this->_fixedPointVal = raw;
+}
+
+float	Fixed::toFloat(void) const
+{
+	float ret;
+	
+	ret = getRawBits() / pow(2, _sFracBits);
+	return (ret);	
+}
+
+int		Fixed::toInt(void) const
+{
+	int	ret;
+
+	ret = getRawBits() >> _sFracBits;
+	return (ret);
+}
+
 //? Private methods
+
+
+//? External functions
+
+/*
+ * These operators must be overloaded as a global function.
+ * And must make them friend to access private data members of the class.
+ */
+std::ostream& operator<< (std::ostream &out, const Fixed &fixed)
+{
+	float ret;
+
+	ret = fixed._fixedPointVal / pow(2, fixed._sFracBits);
+	out << ret;
+	return (out);
+}
--- c02/ex01/Fixed.hpp
+++ c02/ex01/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: hloke <hloke@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/12 21:04:50 by hloke             #+#    #+#             */
-/*   Updated: 2022/07/12 21:28:11 by hloke            ###   ########.fr       */
+/*   Updated: 2022/07/13 14:03:50 by hloke            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,18 @@
 # include <cmath>
 # include <iostream>
 
+/*
+ * Friend is a function that can access private, protected and public members
+ * of a class.
+ */
 class Fixed
 {
 	public:
 		Fixed();
-		Fixed(const Fixed &f);
-		Fixed& operator= (const Fixed &f);
+		Fixed(const int int_val);
+		Fixed(const float f_val);
+		Fixed(const Fixed &fixed);
+		Fixed& operator= (const Fixed &fixed);
 		~Fixed();
 
 		int		getRawBits(void) const;
@@ -29,9 +35,13 @@ class Fixed
 		float	toFloat(void) const;
 		int		toInt(void) const;
 
+		friend std::ostream& operator<< (std::ostream &out, const Fixed &fixed);
+
 	private:
-		int					_numValue;
+		int					_fixedPointVal;
 		static const int	_sFracBits = 8;
 };
+
+std::ostream& operator<< (std::ostream &out, const Fixed &fixed);
 
 # endif

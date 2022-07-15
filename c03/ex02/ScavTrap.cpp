004b5b9700833589657edebfeb0062fe0cd18255
Han qin Loke <hloke@u82z05s06.42kl.edu.my>
2022-07-15 15:07:12 +0800

    [Addition] c03/ex02 repetitive class inheritance done

---
+++ c03/ex02/ScavTrap.cpp
@@ -0,0 +1,98 @@
+#include "ScavTrap.hpp"
+
+/*
+** ------------------------------- CONSTRUCTOR --------------------------------
+*/
+
+ScavTrap::ScavTrap()
+{
+	std::cout << "ScavTrap: Constructor has been called\n";
+
+}
+
+/*
+ * I've decided to add the scope resolution operator (::) to specify which class
+ * method I'm using.
+ */
+ScavTrap::ScavTrap(std::string const name)
+{
+	std::cout << "ScavTrap: Constructor has been called\n";
+	ClapTrap::setName(name);
+	ClapTrap::setHitPoints(100);
+	ClapTrap::setEnergyPoints(50);
+	ClapTrap::setAttackDamage(20);
+}
+
+/*
+ * It's a good practice to call the copy constructor of the base class.
+ */
+ScavTrap::ScavTrap( const ScavTrap & src ) : ClapTrap(src)
+{
+	std::cout << "ScavTrap: Copy constructor has been called\n";
+}
+
+
+/*
+** -------------------------------- DESTRUCTOR --------------------------------
+*/
+
+/*
+ * The destructors are called in exactly the reverse order of the constructors
+ * because of potential dependencies. Assume that the base-class subobject is
+ * still available for use.
+ */
+ScavTrap::~ScavTrap()
+{
+	std::cout << "ScavTrap: Destructor has been called\n";
+}
+
+
+/*
+** --------------------------------- OVERLOAD ---------------------------------
+*/
+
+ScavTrap &				ScavTrap::operator=( ScavTrap const & rhs )
+{
+	std::cout << "ScavTrap: Copy assignment operator has been called\n";
+	if ( this != &rhs )
+	{
+		this->ClapTrap::operator=(rhs);
+	}
+	return *this;
+}
+
+// std::ostream &			operator<<( std::ostream & o, ScavTrap const & i )
+// {
+// 	//o << "Value = " << i.getValue();
+// 	return o;
+// }
+
+
+/*
+** --------------------------------- METHODS ----------------------------------
+*/
+
+/*
+ * The base-class method is shadowed by the derived class method. To use the
+ * base-class method I need to use scope resolution operator (::).
+ * eg: hero.ClapTrap::attack("random")
+ */
+void	ScavTrap::attack		( const std::string & target )
+{
+	std::cout << "ScavTrap " << ClapTrap::getName() 
+			  << " attacks " << target
+			  << ", causing " << ClapTrap::getAttackDamage() 
+			  << " points of damage!\n";
+}
+
+void	ScavTrap::guardGate		()
+{
+	std::cout << "ScavTrap is now in Gatekeeper mode.\n";
+}
+
+/*
+** --------------------------------- ACCESSOR ---------------------------------
+*/
+
+
+/* ************************************************************************** */
---
+++ c03/ex02/main.cpp
@@ -0,0 +1,33 @@
+#include "ClapTrap.hpp"
+#include "FragTrap.hpp"
+#include "ScavTrap.hpp"
+
+void	action(FragTrap &frag)
+{
+	std::cout << " [STATS]\n";
+	std::cout << "  Name: " << frag.getName() << std::endl;
+	std::cout << "    HP: " << frag.getHitPoints() << std::endl;
+	std::cout << "Energy: " << frag.getEnergyPoints() << std::endl;
+	std::cout << "AttDmg: " << frag.getAttackDamage() << std::endl;
+
+	std::cout << " [SKILLS]\n";
+	std::cout << "1: Attack\n"; 
+	std::cout << "Example: "; 
+	frag.attack("FOE");
+	std::cout << "2: High five\n";
+	std::cout << "Example: ";
+	frag.highFivesGuys();
+	std::cout << "\n";
+}
+
+int main(void)
+{
+	FragTrap frag("FRAGGER");  // Constructor
+	FragTrap frag_dup1(frag);  // Copy constructor
+	FragTrap frag_dup2;      
+	frag_dup2 = frag;          // Copy assignment operator
+
+	action(frag);
+	action(frag_dup1);
+	action(frag_dup2);
+}

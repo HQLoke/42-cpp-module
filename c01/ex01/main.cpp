24ea5d00f1f372ab6f20acd013d49b1beffd9de4
Han qin Loke <hloke@u82z05s06.42kl.edu.my>
2022-08-12 13:28:55 +0800

    [Refactoring] c01/ex01

--- c01/ex01/main.cpp
+++ c01/ex01/main.cpp
@@ -1,31 +1,31 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: hloke <hloke@student.42kl.edu.my>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/07/08 15:06:51 by hloke             #+#    #+#             */
-/*   Updated: 2022/07/11 12:03:03 by hloke            ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "Zombie.hpp"
 
 int main(void)
 {
-	Zombie *wild;
+	Zombie	*wild;
+	int		size;
 
-	wild = zombieHorde(10, "First horde");
-	for (int i = 0; i < 10; i += 1)
+	std::cout << "|----------------------------|" << std::endl;
+	std::cout << "|       First stampede       |" << std::endl;
+	std::cout << "|----------------------------|" << std::endl;
+	size = 20;
+	wild = zombieHorde(size, "First stampede");
+	for (int i = 0; i < size; i += 1)
 	{
+		std::cout << "Zombie number " << i + 1 << ": ";
 		wild[i].announce();
 	}
 	delete[] wild;
 
-	wild = zombieHorde(10, "Second uprising");
-	for (int i = 0; i < 10; i += 1)
+	std::cout << std::endl;
+	std::cout << "|-----------------------------|" << std::endl;
+	std::cout << "|       Second uprising       |" << std::endl;
+	std::cout << "|-----------------------------|" << std::endl;
+	size = 42;
+	wild = zombieHorde(size, "Second uprising");
+	for (int i = 0; i < size; i += 1)
 	{
+		std::cout << "Zombie number " << i + 1<< ": ";
 		wild[i].announce();
 	}
 	delete[] wild;

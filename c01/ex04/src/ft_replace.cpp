edcd0bb9ea7ce16d9d6173f597f968825022177c
Han qin Loke <hloke@u91z08s03.42kl.edu.my>
2022-08-15 15:37:26 +0800

    [Bugfix] c01/ex04 forgot to reset index back to 0

--- c01/ex04/src/ft_replace.cpp
+++ c01/ex04/src/ft_replace.cpp
@@ -8,9 +8,9 @@ void	ft_replace(char **av, std::ifstream &inFile, std::ofstream &outFile)
 	size_t			found;
 	size_t			index;
 
-	index = 0;
 	while (std::getline(inFile, content))
 	{
+		index = 0;
 		while ((found = content.find(s1, index)) != std::string::npos)
 		{
 			content.erase(found, s1.length());

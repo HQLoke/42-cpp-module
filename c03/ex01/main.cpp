284d351db3e9b14590fed0144fbff5ff2c1440b9
Han qin Loke <hloke@u91z08s03.42kl.edu.my>
2022-08-16 12:17:00 +0800

    [Refactoring] c03/ex01 added colours to claptrap and scavtrap outputs

--- c03/ex01/main.cpp
+++ c03/ex01/main.cpp
@@ -1,34 +1,31 @@
 #include "ClapTrap.hpp"
 #include "ScavTrap.hpp"
 
-void	action(ScavTrap &scav)
-{
-	std::cout << " [STATS]\n";
-	std::cout << "  Name: " << scav.getName() << std::endl;
-	std::cout << "    HP: " << scav.getHitPoints() << std::endl;
-	std::cout << "Energy: " << scav.getEnergyPoints() << std::endl;
-	std::cout << "AttDmg: " << scav.getAttackDamage() << std::endl;
-
-	std::cout << " [SKILLS]\n";
-	std::cout << "1: Attack\n"; 
-	std::cout << "Example: "; 
-	scav.attack("FOE");
-	std::cout << "2: Guard gate\n";
-	std::cout << "Example: ";
-	scav.guardGate();
-	std::cout << "\n";
-}
-
 int main(void)
 {
-	ScavTrap blank;			   // Blank constructor
-	ScavTrap scav("GUARDIAN"); // String constructor
-	ScavTrap scav_dup1(scav);  // Copy constructor
-	ScavTrap scav_dup2;      
-	scav_dup2 = scav;          // Copy assignment operator
+	ScavTrap	actor("JOHNNY DEPP");
+	ScavTrap	actress("AMBER HEARD");
 
-	action(blank);
-	action(scav);
-	action(scav_dup1);
-	action(scav_dup2);
+	std::cout << actor << std::endl;
+	std::cout << actress << std::endl;
+
+	actor.attack(actress.getName());
+	actress.takeDamage(actor.getAttackDamage());
+
+	std::cout << actor << std::endl;
+	std::cout << actress << std::endl;
+
+	for (int i = 0; i < 50; i += 1)
+	{
+		actress.attack(actor.getName());
+		actor.takeDamage(actress.getAttackDamage());
+	}
+
+	actor.beRepaired(1000);
+	actor.setAttackDamage(1000);
+	actor.attack(actress.getName());
+	actress.takeDamage(actor.getAttackDamage());
+
+	std::cout << actor << std::endl;
+	std::cout << actress << std::endl;
 }
---
+++ c03/ex01/src/ClapTrap.cpp
@@ -0,0 +1,187 @@
+#include "ClapTrap.hpp"
+#include "colour.h"
+
+/*
+** ------------------------------- CONSTRUCTOR --------------------------------
+*/
+
+ClapTrap::ClapTrap()
+{
+	std::cout << RED;
+	std::cout << "ClapTrap: Blank constructor has been called" << std::endl;
+	this->_name = "";
+	this->_hitPoints = 10;
+	this->_energyPoints = 10;
+	this->_attackDamage = 0;
+	std::cout << RESET;
+}
+
+ClapTrap::ClapTrap( std::string const name ) 
+{
+	std::cout << RED;
+	std::cout << "ClapTrap: String constructor has been called" << std::endl;
+	this->_name = name;
+	this->_hitPoints = 10;
+	this->_energyPoints = 10;
+	this->_attackDamage = 0;
+	std::cout << RESET;
+}
+
+ClapTrap::ClapTrap( const ClapTrap & src )
+{
+	std::cout << RED;
+	std::cout << "ClapTrap: Copy constructor has been called" << std::endl;
+	this->_name = src._name;
+	this->_hitPoints = src._hitPoints;
+	this->_energyPoints = src._energyPoints;
+	this->_attackDamage = src._attackDamage;
+	std::cout << RESET;
+}
+
+
+/*
+** -------------------------------- DESTRUCTOR --------------------------------
+*/
+
+ClapTrap::~ClapTrap()
+{
+	std::cout << RED;
+	std::cout << "ClapTrap: Destructor has been called" << std::endl;
+	std::cout << RESET;
+}
+
+
+/*
+** --------------------------------- OVERLOAD ---------------------------------
+*/
+
+ClapTrap &			ClapTrap::operator=( ClapTrap const & rhs )
+{
+	std::cout << RED;
+	std::cout << "ClapTrap: Copy assignment operator has been called" << std::endl;
+	if ( this != &rhs )
+	{
+		this->_name = rhs._name;
+		this->_hitPoints = rhs._hitPoints;
+		this->_energyPoints = rhs._energyPoints;
+		this->_attackDamage = rhs._attackDamage;
+	}
+	std::cout << RESET;
+	return *this;
+}
+
+std::ostream &		operator<<( std::ostream & o, ClapTrap const & i )
+{
+	std::cout << RED;
+	o << "[Stats]\n" 
+	  << "Name  : " << i.getName() << "\n"
+	  << "HP    : " << i.getHitPoints() << "\n"
+	  << "EP    : " << i.getEnergyPoints() << "\n"
+	  << "AttDmg: " << i.getAttackDamage();
+	std::cout << RESET;
+	return o;
+}
+
+
+/*
+** --------------------------------- METHODS ----------------------------------
+*/
+
+static void	deathMessage(std::string const name)
+{
+	std::cout << RED;
+	std::cout << "ClapTrap " << name << " RIP...Stop bothering him!" << std::endl;
+	std::cout << RESET;
+}
+
+void	ClapTrap::attack( const std::string & target )
+{
+	if (this->_hitPoints <= 0)
+		return (deathMessage(this->_name));
+	std::cout << RED;
+	if (this->_energyPoints > 0)
+	{
+		this->_energyPoints -= 1;
+		std::cout << "ClapTrap " << this->_name << " attacks " << target
+		<< ", causing " << this->_attackDamage << " points of damage!" << std::endl;
+	}
+	else
+	{
+		std::cout << "ClapTrap " << this->_name << " has no energy point left"
+		<< "...Everyone, let's start clapping for this useless bastard!" 
+		<< std::endl;
+	}
+	std::cout << RESET;
+}
+
+void	ClapTrap::takeDamage( unsigned int amount )
+{
+	if (this->_hitPoints <= 0)
+		return (deathMessage(this->_name));
+	std::cout << RED;
+	std::cout << "ClapTrap " << this->_name << " took " << amount
+	<< " points of damage!" << std::endl;
+	this->_hitPoints -= amount;
+	if (this->_hitPoints <= 0)
+	{
+		this->_hitPoints = 0;
+		std::cout << "ClapTrap " << this->_name << " has been defeated! "
+		<< "Bye bye loser!" << std::endl;
+	}
+	std::cout << RESET;
+}
+
+void	ClapTrap::beRepaired( unsigned int amount )
+{
+	std::cout << RED;
+	this->_hitPoints += amount;
+	std::cout << "ClapTrap repairs " << amount << " points of damage!" << std::endl;
+	if (this->_hitPoints == (int)amount)
+	{
+		std::cout << "ClapTrap " << this->_name << " is back from the dead!"
+		<< std::endl;
+	}
+	std::cout << RESET;
+}
+
+/*
+** --------------------------------- ACCESSOR ---------------------------------
+*/
+
+std::string	ClapTrap::getName() const
+{
+	return (this->_name);
+}
+
+int			ClapTrap::getHitPoints() const
+{
+	return (this->_hitPoints);
+}
+
+int			ClapTrap::getEnergyPoints() const
+{
+	return (this->_energyPoints);
+}
+
+int			ClapTrap::getAttackDamage() const
+{
+	return (this->_attackDamage);
+}
+
+void		ClapTrap::setHitPoints	( unsigned int amount )
+{
+	this->_hitPoints = amount;
+}
+
+void		ClapTrap::setEnergyPoints ( unsigned int amount )
+{
+	this->_energyPoints = amount;
+}
+
+void		ClapTrap::setAttackDamage ( unsigned int amount )
+{
+	this->_attackDamage = amount;
+}
+
+
+/* ************************************************************************** */

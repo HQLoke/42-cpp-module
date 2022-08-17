52dc21f7a4610a47cafe465859e7a40e82517c75
Han qin Loke <hloke@u91z08s03.42kl.edu.my>
2022-08-17 09:56:25 +0800

    [Refactoring] c05/ex00

--- c05/ex00/main.cpp
+++ c05/ex00/main.cpp
@@ -2,9 +2,34 @@
 
 int main(void)
 {
-	Bureaucrat wtf(151, "WTF");
-	wtf.setGrade(10);
-	wtf.gradeIncrement(9);
-	std::cout << wtf.getGrade() << std::endl;
+	// Bureaucrat a("NAJIB", -44444); // GradeTooHighException
+	// Bureaucrat b("ROSMAH", 44444); // GradeTooLowException
+	Bureaucrat c("NOBODY", 42);
+
+	c.setGrade(10);
+	std::cout << "Current grade: " << c.getGrade() << std::endl;
+	c.gradeIncrement(9);
+	std::cout << "Current grade: " << c.getGrade() << std::endl;
+
+	try
+	{
+		c.gradeIncrement(1);
+	}
+	catch (std::exception & e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+
+	try
+	{
+		c.gradeDecrement(150);
+	}
+	catch (std::exception & e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+
+	std::cout << "Current grade: " << c.getGrade() << std::endl;
+
 	return (0);
 }
